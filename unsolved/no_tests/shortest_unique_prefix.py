7ddb17a3163ac779ccc9559c00f699d6188ee2c7
Siow Yi Sheng <siowyisheng@gmail.com>
2019-01-02 09:58:25 +0800

    adds problems

---
+++ unsolved/no_tests/shortest_unique_prefix.py
@@ -0,0 +1,15 @@
+Given a list of words, return the shortest unique prefix of each word. For
+example, given the list:
+
+    dog
+    cat
+    apple
+    apricot
+    fish
+    Return the list:
+
+        d
+        c
+        app
+        apr
+        f
